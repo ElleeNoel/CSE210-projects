9beb8d50ef45de1c302ca135809f6d1d7829d86a
Ellee Noel <elleeland22@gmail.com>
2026-01-10 22:30:34 -0700

    Added the grading system code.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,42 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What was your grade?");
+        string userGrade = Console.ReadLine();
+        int gradeNumber = int.Parse(userGrade);
+        string gradeLetter = "";
+
+        if (gradeNumber >= 90)
+        {
+            gradeLetter = "A";
+        }
+        else if (gradeNumber >= 80)
+        {
+            gradeLetter = "B";
+        }
+        else if (gradeNumber >= 70)
+        {
+            gradeLetter = "C";
+        }
+        else if (gradeNumber >= 60)
+        {
+            gradeLetter = "D";
+        }
+        else if (gradeNumber < 60)
+        {
+            gradeLetter = "F";
+        }
+
+        Console.WriteLine($"You got a {gradeLetter}");
+
+
+        if (gradeNumber >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed!");
+        }
+        else
+        {
+            Console.WriteLine("You failed. Better luck next time!");
+        }
     }
 }
