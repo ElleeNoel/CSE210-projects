03f565ff19d8c4387eb57a695a609fef96740ddd
Ellee Noel <elleeland22@gmail.com>
2026-02-17 08:33:30 -0700

    Created Assgnment class and writing and math subclasses.

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -5,5 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning04 World!");
+
+        MathAssignment myMathHomework = new MathAssignment("Section 3.5", "Problems 17-25", "Ellee Land", "Fractions");
+        Console.WriteLine(myMathHomework.GetSummary());
+        Console.WriteLine(myMathHomework.GetHomeworkList());
+
+        WritingAssignment myEssay = new WritingAssignment("Heroes of Celtic Mythology", "Taran Wanderer", "Celtic Mythology");
+        Console.WriteLine(myEssay.GetSummary());
+        Console.WriteLine(myEssay.GetWritingInfo());
     }
 }
