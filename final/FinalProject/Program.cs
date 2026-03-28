007716e6dabf47f7e18c17830d4ea5f8ecc3eda9
Ellee Noel <elleeland22@gmail.com>
2026-03-27 21:58:35 -0600

    Main program class: not finished. I think the practical code is done, I just need to add flavor stuff and make it pretty.

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello FinalProject World!");
+        // the is where the "main" for the Kronicle boss fight will be
+
+        // I guess just istantiate the fight and do a bit of flavor stuff before?
+        Player player = new Player();
+        BaronWight baronWight = new BaronWight();
+        Fight fight = new Fight(player, baronWight);
+        fight.BossFight();
     }
 }
