ed5bad1ef53eef55b6baaaf6da1881e160edc662
Ellee Noel <elleeland22@gmail.com>
2026-04-02 12:52:58 -0600

    I built the constructor, added some print statements so the user can tell what is going on when they use an item, and made the input validation not case specific because that was causing problems. Everything works now.

--- final/FinalProject/Inventory.cs
+++ final/FinalProject/Inventory.cs
@@ -6,14 +6,14 @@ public class Inventory
     {
         // in a full version this would import the info from a save file, but for now we'll
         // set it to a default
-        items.Add("Bubblegum");
-        items.Add("Bubblegum");
-        items.Add("Timestream Syrup");
-        items.Add("Bubblegum");
-        items.Add("Bubblegum");
-        items.Add("Timestream Syrup");
-        items.Add("Bubblegum");
-        items.Add("Timestream Syrup");
+        items.Add("BUBBLEGUM");
+        items.Add("BUBBLEGUM");
+        items.Add("TIMESTREAM SYRUP");
+        items.Add("BUBBLEGUM");
+        items.Add("BUBBLEGUM");
+        items.Add("TIMESTREAM SYRUP");
+        items.Add("BUBBLEGUM");
+        items.Add("TIMESTREAM SYRUP");
     }
 
     public void DisplayInventory()
@@ -29,18 +29,20 @@ public class Inventory
         int statIncrease = 0;
         foreach (string item in items)
         {
-            if (selectedItem == item)
+            if (selectedItem.ToUpper() == item)
             {
                 // can break loop then return, or return from loop
                 items.Remove(item);
-                if (selectedItem == "Bubblegum")
+                if (selectedItem.ToUpper() == "BUBBLEGUM")
                 {
                     statIncrease =  5;
+                    Console.WriteLine("You recoverd 5 HP!");
                     break;
                 }
                 else
                 {
                     statIncrease =  3;
+                    Console.WriteLine("You recovered 3 MP!");
                     break;
                 }
             }
