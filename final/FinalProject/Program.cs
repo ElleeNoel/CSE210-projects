534a93f615df0408cac9f931cfa190a98b8b8be9
Ellee Noel <elleeland22@gmail.com>
2026-04-02 12:49:41 -0600

    I typed up a game start pseudo-UI (printed in console). Includes instructions for the user.

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -8,6 +8,24 @@ class Program
         // the is where the "main" for the Kronicle boss fight will be
 
         // I guess just istantiate the fight and do a bit of flavor stuff before?
+        Console.WriteLine("KRONICLE BOSS FIGHT");
+        Console.WriteLine();
+        Console.WriteLine("Welcome to the game! Press enter to start.");
+        Console.ReadLine();
+        Console.WriteLine();
+        Console.WriteLine("INSTRUCTIONS: This is a text-based RPG-style fight game. You will ");
+        Console.WriteLine("be prompted for an input any time it is your turn to take an action. When ");
+        Console.WriteLine("this happens, you should type one of the options provided with the prompt. ");
+        Console.WriteLine();
+        Console.WriteLine("BACKGROUND: You've come to the end of an arduous and wacky adventure ");
+        Console.WriteLine("through time. Your party consists of: KRONIX, a modern teenager with a ");
+        Console.WriteLine("sword and a magical pocketwatch he found in his father's inventing workshop; ");
+        Console.WriteLine("EMILY, a dancer from the Age of the Sword; JULIAN, a sharpshooter with ");
+        Console.WriteLine("a short temper from the Age of Lights; and HUBERT, an amateur wizard ");
+        Console.WriteLine("with an encyclopedic knowledge of the world and a sarcastic sense of humor.");
+        Console.WriteLine();
+        Console.WriteLine("Press enter when you're ready to start.");
+        Console.ReadLine();
         Player player = new Player();
         BaronWight baronWight = new BaronWight();
         Fight fight = new Fight(player, baronWight);
